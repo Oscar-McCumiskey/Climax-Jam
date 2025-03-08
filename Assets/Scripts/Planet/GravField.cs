3df603f5fc4e7f79187d216ee1afedc40018d84d
itselliexo <elliemjoliver@gmail.com>
2025-03-08 20:16:14 +0000

    Level 1 blockout

--- Assets/Scripts/Planet/GravField.cs
+++ Assets/Scripts/Planet/GravField.cs
@@ -1,7 +1,35 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class GravField : MonoBehaviour
 {
     public float gravity;
+    [SerializeField] SpriteRenderer gravField;
+    [SerializeField] Color weakColour;
+    [SerializeField] Color strongColour;
 
+    private void SetColourBasedOnGravity()
+    {
+        float adjustedGravity = Mathf.Clamp(gravity, 0f, 10f);
+
+        float normalizedGravity = Mathf.InverseLerp(3f,6f, adjustedGravity);
+
+        Color colourBasedOnGrav = Color.Lerp(weakColour, strongColour, normalizedGravity);
+
+        float hue, saturation, value;
+        Color.RGBToHSV(colourBasedOnGrav, out hue, out saturation, out value);
+
+        saturation = 0.4f;
+        value = 1f;
+
+        colourBasedOnGrav = Color.HSVToRGB(hue, saturation, value);
+
+        colourBasedOnGrav.a = 1f;
+        gravField.color = colourBasedOnGrav;
+    }
+
+    private void Awake()
+    {
+        SetColourBasedOnGravity();
+    }
 }
